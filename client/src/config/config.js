f68d7928f7a16562131d9930fd2dd5f553818a88
Adalbert Agheorghesei <agheorgheseiadalbert@gmail.com>
2023-09-05 11:25:55 +0100

    fix api link

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -2,6 +2,9 @@ const config = {
   development: {
     backendUrl: "http://localhost:8080/api/v1/dalle",
   },
+  production: {
+    backendUrl: "https://ai-3d-website.netlify.app//api/v1/dalle",
+  },
 };
 
 export default config;
